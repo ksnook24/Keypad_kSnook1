089d1b829c247867f45d961c723296f0b95a8de1
ksnook24 <63332685+ksnook24@users.noreply.github.com>
2021-03-13 23:47:19 -0800

    Initial commit

---
+++ Assets/Scripts/KeypadBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeypadBackground : MonoBehaviour
+{
+    //public KeypadBackground Background;
+
+    public GameObject UnlockButton;
+    public Image BackgroundImage;
+
+    public void HideUnlockButton()
+    {
+        UnlockButton.SetActive(false);
+    }
+
+    public void ChangeToSuccessColor()
+    {
+        BackgroundImage.color = Color.green;
+    }
+    public void ChangeToFailedColor()
+    {
+        BackgroundImage.color = Color.red;
+    }
+    public void ChangeToDefaultColor()
+    {
+        BackgroundImage.color = Color.grey;
+    }
+}
---
+++ Assets/Scripts/KeypadButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadButton : MonoBehaviour
+{
+    public int ButtonValue;
+    public Keypad Keypad;
+
+    public void OnClick()
+    {
+        Keypad.RegisterButtonClick(ButtonValue);
+    }
+
+}
